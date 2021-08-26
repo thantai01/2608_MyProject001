be701a33f9cb4ffdb98a17680da8a43d20775c77
thantai01 <thantaihp2011@gmail.com>
2021-08-26 22:09:58 +0700

    update categories api

---
+++ src/main/java/com/personal/management/controller/CategoryAPIController.java
@@ -0,0 +1,46 @@
+package com.personal.management.controller;
+
+import com.personal.management.model.Category;
+import com.personal.management.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class CategoryAPIController {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @GetMapping("/categories")
+    ResponseEntity<Iterable<Category>> findAll() {
+        return new ResponseEntity<>(categoryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/categories")
+    ResponseEntity<Category> save(@RequestBody Category category) {
+        categoryRepository.save(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    ResponseEntity<Void> delete(@PathVariable long id) {
+        categoryRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/categories/{id}")
+    ResponseEntity<Optional<Category>> edit (@PathVariable long id, Category category) {
+        Optional<Category> selected = categoryRepository.findById(id);
+        if(selected.isPresent()) {
+            category.setId(id);
+            categoryRepository.save(category);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
